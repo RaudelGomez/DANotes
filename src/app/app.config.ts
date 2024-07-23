868b94e9e5157c7fb603d693f7ca66b42ea7fbdc
Raudel Gómez Smith <gomezsmith.raudel@gmail.com>
2024-07-23 16:54:51 +0200

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"danotes-661f2","appId":"1:693891937237:web:ff1dc4c05fbacc1e6c435c","storageBucket":"danotes-661f2.appspot.com","apiKey":"AIzaSyBTBljEVRIaFhztF4XQjbJzlbK7guYaUpU","authDomain":"danotes-661f2.firebaseapp.com","messagingSenderId":"693891937237"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
